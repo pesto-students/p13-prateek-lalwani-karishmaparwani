73589822efb6d28eb4fa37fae842575f2c344ea3
Karishma <kthakkar540@gmail.com>
2023-09-06 15:25:59 +0530

    call_bind_apply

--- Week-4/JS Scopes,Context/call_bind_apply.js
+++ Week-4/JS Scopes,Context/call_bind_apply.js
@@ -1,3 +1,54 @@
 class Calculator {
-    
+    add(a, b) {
+        return console.log(a + b)
+    }
+
+    subsract(a, b) {
+        return console.log(a - b)
+    }
+
+    multiply(a, b) {
+        return console.log(a * b)
+    }
+
+    divide(a, b) {
+        return console.log(a / b)
+    }
 }
+
+class ScientificCalculator extends Calculator {
+
+    square(a) {
+        return console.log(a * a)
+    }
+
+    power(a, b) {
+        return console.log(a ** b)
+    }
+
+    cube(a) {
+        return console.log(a * a * a)
+    }
+}
+
+const scientificCalculatorInstance = new ScientificCalculator();
+
+scientificCalculatorInstance.add.call(this,10,5);
+
+scientificCalculatorInstance.subsract.apply(this,[10,5]);
+
+function multiplyByTwo(number){
+    return  number * 2;
+}
+
+const bindExample = multiplyByTwo.bind(scientificCalculatorInstance,4)
+
+console.log(bindExample())
+
+function powerOfThree(number){
+    return  number ** 3
+}
+
+const bindExample2 = powerOfThree.bind(scientificCalculatorInstance,4)
+
+console.log(bindExample2())
