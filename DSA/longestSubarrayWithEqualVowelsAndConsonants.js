c8f7920e1789189857ddfda582fca51db09d799c
Karishma <kthakkar540@gmail.com>
2023-10-06 19:56:07 +0530

    minor corrections

--- DSA/longestSubarrayWithEqualVowelsAndConsonants.js
+++ DSA/longestSubarrayWithEqualVowelsAndConsonants.js
@@ -1,3 +1,19 @@
+/**
+ * Write a function that takes an array of characters as input and finds the length of the longest subarray in the array that contains an equal number of vowels and consonants. The vowels are defined as 'a', 'e', 'i', 'o', and 'u' (both lowercase and uppercase).
+
+Example
+Input
+The input is taken as a prompt from the user. The user is prompted to enter a string.
+Output
+The function should return the length of the longest subarray that contains an equal number of vowels and consonants.
+Explanation
+Input
+babadabcdeiouiop
+Output
+8
+ * @param {*} arr 
+ * @returns 
+ */
 function longestSubarrayWithEqualVowelsAndConsonants(arr){
     let vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
     let vowelsMap = new Map();
@@ -21,4 +37,4 @@ function longestSubarrayWithEqualVowelsAndConsonants(arr){
 }
 
 const input = "babadabcdeiouiop";
-console.log(longestSubarrayWithEqualVowelsAndConsonants(input)); // Output: 8
+console.log(longestSubarrayWithEqualVowelsAndConsonants(input)); // Output: 12
