9df84a70a437bf6d8d7890a59782f7ad688a8db7
Karishma <kthakkar540@gmail.com>
2023-10-17 00:50:28 +0530

    Adding two pointersExample

---
+++ DSA/Two Pointer/twoSum.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let left = 0;
+    let right = numbers.length - 1;
+
+    while(left < right){
+        const sum = numbers[left] + numbers[right];
+
+        if(sum == target){
+            return [left +1 , right + 1];
+        }else if (sum < target ){
+            left ++;
+        }else{
+            right --;
+        }
+    }
+    return []
+};
+
+const input = [2, 7, 11, 15];
+const target = 9;
+const result = twoSum(input, target);
+console.log(result); // Output: [1, 2]
